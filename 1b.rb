416d9f448dcb015a4c41414046336bc19bd8b3f8
chris escue <chris@escue.io>
2022-12-01 19:56:01 -0600

    Make sure the last elf's calories are included
    Sneaky little bugger.

--- 1b.rb
+++ 1b.rb
@@ -16,4 +16,7 @@ File.foreach('1_input.txt') do |line|
   current_elf_calories += line.chomp.to_i
 end
 
+most_calories << current_elf_calories
+most_calories = most_calories.sort![1..3]
+
 puts most_calories.inject(&:+)
